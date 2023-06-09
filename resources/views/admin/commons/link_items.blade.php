7c441d1488f726cd7502542c07868d9f39c91550
ryousuke.mizokami <ming10r1170@gmail.com>
2023-06-09 12:22:50 +0000

    admin login registerデザイン変更

---
+++ resources/views/admin/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- チーム詳細ページへのリンク --}}
+    <li><a class="link link-hover" href="#">チームprofile</a></li>
+    {{-- メンバー詳細ページへのリンク --}}
+    <li><a class="link link-hover" href="#">member</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="route('admin.logout')" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('admin.register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('admin.login') }}">Login</a></li>
+@endif
